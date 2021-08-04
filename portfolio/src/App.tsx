1649c0993fc94246b4336a1a90b4a4291fc2302c
stephenlyons18 <stephen.lyons18@gmail.com>
2021-08-04 15:56:18 -0700

    moved some things around

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,58 +1,16 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
-import Home from './Pages/Home/Home'
-import About from './Pages/About/About'
-import Projects from './Pages/Projects/Projects'
-import { AnimatePresence } from 'framer-motion';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from 'react-router-dom';
+
 import { color } from 'style-value-types'
+import NavBar from './components/NavBar/NavBar';
+import RouterWrap from './components/Router/RouterWrap';
 
 class App extends Component {
   render(){
   return (
-    <Router>
-      <div>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/about">About</Link>
-          </li>
-          <li>
-            <Link to="/projects">Projects</Link>
-          </li>
-        </ul>
-
-        <hr />
-
-        {/*
-          A <Switch> looks through all its children <Route>
-          elements and renders the first one whose path
-          matches the current URL. Use a <Switch> any time
-          you have multiple routes, but you want only one
-          of them to render at a time
-        */}
-        <AnimatePresence exitbeforeenter>
-        <Switch>
-          <Route exact path="/">
-            <Home text='hello world' onTap={console.log('hello world')} style={{color:'blue'}} />
-          </Route>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/projects">
-            <Projects text='hello world2' onTap={console.log('hello world2')} style={{color:'blue'}} />
-          </Route>
-        </Switch>
-        </AnimatePresence>
-      </div>
-    </Router>
+    <div>
+      <RouterWrap/>
+      <NavBar/>
+    </div>
   );
   }
 }
---
+++ portfolio/src/components/NavBar/NavBar.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from 'react-router-dom';
+import Home from '../../Pages/Home/Home';
+import Projects from '../../Pages/Projects/Projects';
+import About from '../../Pages/About/About';
+import { AnimatePresence } from 'framer-motion';
+const NavBar = () => {
+    return (
+        <div>
+            <Router>
+            <div  >
+                <ul>
+                    <li>
+                        <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                        <Link to="/about">About</Link>
+                    </li>
+                    <li>
+                        <Link to="/projects">Projects</Link>
+                    </li>
+                </ul>
+
+                <hr />
+
+                {/*
+                A <Switch> looks through all its children <Route>
+                elements and renders the first one whose path
+                matches the current URL. Use a <Switch> any time
+                you have multiple routes, but you want only one
+                of them to render at a time
+                */}
+                <AnimatePresence>
+                    <Switch>
+                        <Route exact path="/">
+                            <Home text='hello world' onTap={console.log('hello world')} style={{color:'blue'}} />
+                        </Route>
+                        <Route path="/about">
+                            <About />
+                        </Route>
+                        <Route path="/projects">
+                            <Projects text='hello world2' onTap={console.log('hello world2')} style={{color:'blue'}} />
+                        </Route>
+                    </Switch>
+                </AnimatePresence>
+            </div>
+            </Router>
+        </div>
+    )
+}
+
+export default NavBar
