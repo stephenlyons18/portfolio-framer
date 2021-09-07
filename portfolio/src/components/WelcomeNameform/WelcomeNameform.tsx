d1c638f6478b216a0c440fe47a589785684e15e0
Stephen Lyons <55640562+stephenlyons18@users.noreply.github.com>
2021-09-06 23:52:04 -0700

    changes to text input

--- portfolio/src/components/WelcomeNameform/WelcomeNameform.tsx
+++ portfolio/src/components/WelcomeNameform/WelcomeNameform.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import { CaretRight } from 'phosphor-react';
+import '../../styles/styles.css';
+
+const variants = {
+    typed: { opacity: 1 },
+    notTyped: { opacity: 0.1 },
+};
+const caretVariants = {
+    on: { opacity: 1 },
+    off: { opacity: 0 },
+};
+const inputStyle = {
+    backgroundColor: 'transparent',
+    border: 'none',
+    height: '30vh',
+    width: '30vw',
+    fontSize: '80px',
+};
+const flexContainer = {
+    display: 'flex',
+    alignItems: 'center',
+};
+const containerStyle = {
+    // display: 'flex',
+    // alignItems: 'center',
+};
 
 class WelcomeNameform extends React.Component<
     {},
@@ -21,18 +47,33 @@ class WelcomeNameform extends React.Component<
     };
     render() {
         return (
-            <motion.div variants={variants} initial="notTyped" animate="typed">
-                <input
-                    type="text"
-                    value={this.state.text}
-                    onChange={this.onChange}
-                />
+            <motion.div
+                variants={variants}
+                initial="notTyped"
+                animate="typed"
+                style={containerStyle}
+            >
+                <h1>Type your name here</h1>
+                <br />
+                <div style={flexContainer}>
+                    <motion.div
+                        variants={caretVariants}
+                        initial="on"
+                        animate="off"
+                    >
+                        <CaretRight size={48} />
+                    </motion.div>
+
+                    <input
+                        type="text"
+                        value={this.state.text}
+                        onChange={this.onChange}
+                        style={inputStyle}
+                    />
+                </div>
             </motion.div>
         );
     }
 }
-const variants = {
-    typed: { opacity: 1 },
-    notTyped: { opacity: 0.1 },
-};
+
 export default WelcomeNameform;
