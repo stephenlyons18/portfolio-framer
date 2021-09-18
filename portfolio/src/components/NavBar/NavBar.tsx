aa5e2468c2a36dc8fd2eea20a4810da4e4519082
stephenlyons18 <stephen.lyons18@gmail.com>
2021-09-17 22:02:53 -0700

    added sass

--- portfolio/src/components/NavBar/NavBar.tsx
+++ portfolio/src/components/NavBar/NavBar.tsx
@@ -1,7 +1,7 @@
 import { motion } from 'framer-motion';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import './NavBar.css';
+import './NavBar.scss';
 import { Chat } from 'phosphor-react';
 import ContactOverlay from '../ContactOverlay/ContactOverlay';
 import { setConstantValue } from 'typescript';
@@ -9,40 +9,51 @@ import { setConstantValue } from 'typescript';
 const NavBar = () => {
     const [open, setOpen] = useState('closed');
     return (
-        <div className="NavContainerStyle">
-            <Link to="/" className="noDecor">
-                {' '}
-                <motion.div whileTap={{ rotate: 10 }} className="NavLinkStyle">
-                    HOME
-                </motion.div>{' '}
-            </Link>
-            <Link to="/projects" className="noDecor">
-                {' '}
-                <motion.div whileTap={{ rotate: 10 }} className="NavLinkStyle">
-                    PROJECTS
-                </motion.div>{' '}
-            </Link>
-            <Link to="/About" className="noDecor">
-                {' '}
-                <motion.div className="NavLinkStyle" whileTap={{ rotate: 10 }}>
-                    ABOUT
+        <nav>
+            <div className="NavContainerStyle">
+                <Link to="/" className="noDecor">
+                    {' '}
+                    <motion.div
+                        whileTap={{ scale: 0.9 }}
+                        className="NavLinkStyle"
+                    >
+                        HOME
+                    </motion.div>{' '}
+                </Link>
+                <Link to="/projects" className="noDecor">
+                    {' '}
+                    <motion.div
+                        whileTap={{ scale: 0.9 }}
+                        className="NavLinkStyle"
+                    >
+                        PROJECTS
+                    </motion.div>{' '}
+                </Link>
+                <Link to="/About" className="noDecor">
+                    {' '}
+                    <motion.div
+                        className="NavLinkStyle"
+                        whileTap={{ scale: 0.9 }}
+                    >
+                        ABOUT
+                    </motion.div>
+                </Link>
+                <motion.div whileTap={{ scale: 0.9 }} className="NavLinkStyle">
+                    <Chat
+                        size={48}
+                        onClick={() => {
+                            if (open === 'closed') {
+                                setOpen('open');
+                            } else {
+                                setOpen('closed');
+                            }
+                        }}
+                    />
                 </motion.div>
-            </Link>
-            <motion.div whileTap={{ scale: 0.9 }} className="NavLinkStyle">
-                <Chat
-                    size={48}
-                    onClick={() => {
-                        if (open === 'closed') {
-                            setOpen('open');
-                        } else {
-                            setOpen('closed');
-                        }
-                    }}
-                />
-            </motion.div>
 
-            <ContactOverlay open="open" />
-        </div>
+                <ContactOverlay open="open" />
+            </div>
+        </nav>
     );
 };
 
