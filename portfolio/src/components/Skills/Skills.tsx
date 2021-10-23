34e5c654987314646b51f37ee7e5ef8fe58cd2cf
stephenlyons18 <stephen.lyons18@gmail.com>
2021-10-23 14:06:21 -0700

    skills improvements

--- portfolio/src/components/Skills/Skills.tsx
+++ portfolio/src/components/Skills/Skills.tsx
@@ -1,63 +1,83 @@
 import React from 'react';
-import { motion, AnimateSharedLayout } from 'framer-motion';
-
+import { motion } from 'framer-motion';
+import './Skills.scss';
 export const Skills = () => {
     return (
-        <motion.div>
-            <AnimateSharedLayout>
-                {skills.map((skill) => {
-                    return (
+        <motion.div
+            className="skillsContainer"
+            variants={containerVariants}
+            initial="hidden"
+            animate="show"
+        >
+            {skills.map((skill) => {
+                return (
+                    <motion.div key={skill.skill}>
+                        <motion.h3 className="skillText">
+                            {skill.skill}
+                        </motion.h3>
                         <motion.div
                             className="skillsBar"
-                            initial={{ width: '0px' }}
-                            animate={{ width: skill.level }}
-                        >
-                            {skill.skill}
-                        </motion.div>
-                    );
-                })}
-                {/* <AnimatePresence>
-                        {selectedId && <motion.img layoutId={selectedId} />}
-                    </AnimatePresence> */}
-            </AnimateSharedLayout>
+                            variants={skillVariants}
+                            initial="closed"
+                            animate="full"
+                            custom={skill}
+                        />
+                    </motion.div>
+                );
+            })}
         </motion.div>
     );
 };
 const skills = [
     {
         skill: 'JavaScript',
-        level: '90px',
+        level: '90%',
     },
     {
         skill: 'Python',
-        level: '75px',
+        level: '75%',
     },
     {
-        skill: 'HTML',
-        level: '85px',
+        skill: 'HTML5',
+        level: '85%',
     },
     {
-        skill: 'Linux',
-        level: '70px',
+        skill: 'Linux / Kali Linux',
+        level: '70%',
     },
     {
         skill: 'Java',
-        level: '65px',
+        level: '65%',
     },
     {
         skill: 'TypeScript',
-        level: '85px',
+        level: '85%',
     },
     {
         skill: 'React.js',
-        level: '80px',
+        level: '80%',
     },
 ];
-// const skillsVariants = {
-//     closed: {
-//         width: 0,
-//     },
-//     open: {
-//         width: item.skill,
-//     },
-// };
+const containerVariants = {
+    hidden: { opacity: 0 },
+    show: {
+        opacity: 1,
+        transition: {
+            delayChildren: 5,
+            staggerChildren: 1,
+        },
+    },
+};
+const skillVariants = {
+    closed: { width: '0px' },
+    full: (skill) => ({
+        width: skill.level,
+        transition: {
+            duration: 5,
+            type: 'spring',
+            stiffness: 566,
+            damping: 32,
+            mass: 3.9,
+        },
+    }),
+};
