968c44866284611acd073266d5ab0fa157ff83a6
Stephen Lyons <55640562+stephenlyons18@users.noreply.github.com>
2022-01-09 01:57:37 -0800

    fixes to projects and swiping navigation implemented

--- src/Pages/Projects/Projects.tsx
+++ src/Pages/Projects/Projects.tsx
@@ -2,9 +2,8 @@ import React, { useState } from 'react';
 import { motion, AnimateSharedLayout, Reorder } from 'framer-motion';
 import { ProjectItems } from './ProjectItems';
 import './ProjectStyles.scss';
-import { Hidden } from '@mui/material';
 
-function Projects() {
+function Projects(props) {
     const [items, setItems] = useState(ProjectItems);
     const [openItem, setOpenItem] = useState(null);
 
@@ -50,7 +49,12 @@ function Projects() {
                 justifyContent: 'center',
             }}
         >
-            <Reorder.Group axis="y" values={items} onReorder={setItems}>
+            <Reorder.Group
+                axis="y"
+                values={items}
+                onReorder={setItems}
+                className="ulStyle"
+            >
                 {items.map((item) => (
                     <Reorder.Item key={item.projectID} value={item}>
                         <motion.div
@@ -72,11 +76,6 @@ function Projects() {
                             <motion.img
                                 src={item.imgSrc}
                                 alt={item.projectTitle}
-                                style={{
-                                    width: '100%',
-                                    height: '300px',
-                                    borderRadius: '20px',
-                                }}
                                 whileHover={
                                     openItem === item.projectID
                                         ? { scale: 1.3 }
@@ -92,13 +91,33 @@ function Projects() {
                             />
                             <h1>{item.projectTitle}</h1>
                             {openItem === item.projectID && (
-                                <div>
-                                    <p>{item.description}</p>
-                                    <ul>
+                                <div className="project-text">
+                                    <h2>Description</h2>
+                                    <motion.p
+                                    // variants={projectTextVariants}
+                                    // initial="closed"
+                                    // animate={
+                                    //     openItem === item.projectID
+                                    //         ? 'open'
+                                    //         : 'closed'
+                                    // }
+                                    >
+                                        {item.description}
+                                    </motion.p>
+                                    <h2>Technologies</h2>
+                                    <motion.ul
+                                        variants={motionULVariants}
+                                        initial="closed"
+                                        animate={
+                                            openItem === item.projectID
+                                                ? 'open'
+                                                : 'closed'
+                                        }
+                                    >
                                         {item.techStack.map((tech) => (
                                             <li key={tech}>{tech}</li>
                                         ))}
-                                    </ul>
+                                    </motion.ul>
                                 </div>
                             )}
                         </motion.div>
@@ -108,14 +127,17 @@ function Projects() {
         </motion.div>
     );
 }
+
 const ProjectCardVariants = {
     hidden: {
         backgroundColor: '#33658a',
         height: 'auto',
         borderRadius: '20px',
-        width: '100%',
-        margin: '20%',
-        padding: '10%',
+        width: '80%',
+        marginTop: '20px',
+        marginBottom: '20px',
+        padding: '20px',
+
         transition: {
             type: 'spring',
             stiffness: 100,
@@ -123,23 +145,46 @@ const ProjectCardVariants = {
         },
     },
     show: {
-        scale: 1.3,
-        padding: '30%',
-        margin: '20%',
+        scale: 1.1,
+        marginBottom: '200px',
+        marginTop: '200px',
         backgroundColor: '#86bbd8',
+        padding: '20px',
+    },
+};
+const motionULVariants = {
+    closed: {
+        x: '-100vw',
+        transition: {
+            type: 'spring',
+            stiffness: 100,
+            damping: 15,
+            staggerChildren: 0.2,
+        },
+    },
+    open: {
+        x: '0',
+        transition: {
+            type: 'spring',
+            stiffness: 100,
+            damping: 15,
+            staggerChildren: 0.2,
+        },
     },
 };
 const ImageVariants = {
     hidden: {
         opacity: 0.7,
-        width: '100%',
+        width: '400px',
+        borderRadius: '20px',
         transition: {
             duration: 0.5,
         },
     },
     show: {
         opacity: 1,
-        width: '100%',
+        borderRadius: '20px',
+
         transition: {
             duration: 0.5,
         },
