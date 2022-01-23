91fbf7c30fb13c030a4b919814076d0c2cb5facf
stephenlyons18 <stephen.lyons18@gmail.com>
2022-01-23 01:05:01 -0800

    navbar and app nav changes. new idea, swipe navigation

--- src/App.tsx
+++ src/App.tsx
@@ -7,59 +7,14 @@ import About from './Pages/About/About';
 import { AnimatePresence } from 'framer-motion';
 import NavBar from './components/NavBar/NavBar';
 import { useState } from 'react';
-import { useSwipeable } from 'react-swipeable';
 
 function App() {
     //const [ContactToggle, setContactToggle] = useState(false);
     const [currentPage, setCurrentPage] = useState(0);
-    const handlers = useSwipeable({
-        onSwipedLeft: (eventData) => swipeLeft(eventData),
-        onSwipedRight: (eventData) => swipeRight(eventData),
-    });
-    let navigate = useNavigate();
-
-    function swipeLeft(eventData) {
-        if (eventData.deltaX > 250) {
-            if (currentPage === 0) {
-                setCurrentPage(2);
-                console.log('swiped left');
-            } else if (currentPage === 1) {
-                setCurrentPage(0);
-            } else if (currentPage === 2) {
-                setCurrentPage(1);
-            }
-            handleSwipe(eventData);
-        }
-    }
-    function swipeRight(eventData) {
-        if (eventData.deltaX > 250) {
-            if (currentPage === 0) {
-                setCurrentPage(1);
-                console.log('swiped right');
-            } else if (currentPage === 1) {
-                setCurrentPage(2);
-            } else if (currentPage === 2) {
-                setCurrentPage(0);
-                console.log('swiped right');
-            }
-            handleSwipe(eventData);
-        }
-    }
-    //use a listener to handle navigation based on which page is currently active
-    const handleSwipe = (e) => {
-        console.log(e);
-        if (currentPage === 0) {
-            navigate('/', { state: { swipeDirection: e.dir } });
-        } else if (currentPage === 1) {
-            navigate('/projects', { state: { swipeDirection: e.dir } });
-        } else if (currentPage === 2) {
-            navigate('/about', { state: { swipeDirection: e.dir } });
-        }
-    };
 
     return (
-        <div {...handlers}>
-            <NavBar />
+        <>
+            <NavBar currentPage={currentPage} setCurrentPage={setCurrentPage} />
             <AnimatePresence exitBeforeEnter>
                 <Routes>
                     <Route path="/" element={<Home />} />
@@ -67,7 +22,7 @@ function App() {
                     <Route path="/about" element={<About />} />
                 </Routes>
             </AnimatePresence>
-        </div>
+        </>
     );
 }
 
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -3,16 +3,65 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import { Chat, LinkedinLogo, GithubLogo, List } from 'phosphor-react';
+import { useSwipeable } from 'react-swipeable';
+import { useNavigate } from 'react-router-dom';
 
-const NavBar = () => {
+interface NavBarProps {
+    currentPage: number;
+    setCurrentPage: (page: number) => void;
+}
+
+function chooseNavigation() {}
+
+const NavBar: React.FC<NavBarProps> = ({ currentPage, setCurrentPage }) => {
     //detect if the device is a mobile device
-    const isMobile = window.innerWidth < 500;
-    const [isMobileOpen, setMobileOpen] = useState('closed');
-    //create a window resize listener and set the state of the isMobile
-    //variable to the new value
+    const [isMobile, setIsMobile] = useState(window.innerWidth < 500);
+
+    //create a window resize listener
     window.addEventListener('resize', () => {
-        isMobile ? setMobileOpen('open') : setMobileOpen('closed');
+        setTimeout(() => {
+            if (window.innerWidth < 500) {
+                setIsMobile(true);
+            } else {
+                setIsMobile(false);
+            }
+        }, 1000);
     });
+
+    const handlers = useSwipeable({
+        onSwipedLeft: (eventData) => swipeLeft(eventData),
+        onSwipedRight: (eventData) => swipeRight(eventData),
+    });
+    let navigate = useNavigate();
+
+    function swipeRight(eventData) {
+        if (eventData.deltaX > 100) {
+            if (currentPage === 0) {
+                navigate('/projects');
+                setCurrentPage(1);
+            } else if (currentPage === 1) {
+                setCurrentPage(2);
+                navigate('/about');
+            } else if (currentPage === 2) {
+                setCurrentPage(0);
+                navigate('/');
+            }
+        }
+    }
+    function swipeLeft(eventData) {
+        if (eventData.deltaX > 100) {
+            if (currentPage === 0) {
+                setCurrentPage(2);
+                navigate('/about');
+            } else if (currentPage === 1) {
+                setCurrentPage(0);
+                navigate('/');
+            } else if (currentPage === 2) {
+                setCurrentPage(1);
+                navigate('/projects');
+            }
+        }
+    }
 
     return !isMobile ? (
         <nav>
@@ -74,68 +123,27 @@ const NavBar = () => {
         </nav>
     ) : (
         <>
-            <div className="mobileNav">
-                <div className="mobileNavContainer">
-                    <div className="mobileNavTitle">
-                        <Link to="/" className="noDecor">
-                            <motion.div
-                                whileTap={{ scale: 0.9 }}
-                                className="NavLinkStyle"
-                            >
-                                <h3 style={{ margin: '0px' }}>Stephen Lyons</h3>
-                            </motion.div>
-                        </Link>
-                    </div>
-                    {/* create a menu button that opens the menu */}
-                    <div className="mobileNavMenu">
+            {/* create a home button on the left, with three nav buttons on the right*/}
+            <div className="flex flex-row justify-center">
+                <div>
+                    <Link to="/" className="noDecor">
                         <motion.div
-                            className="mobileNavMenuButton"
-                            onClick={() => {
-                                setMobileOpen(
-                                    isMobileOpen === 'closed'
-                                        ? 'open'
-                                        : 'closed'
-                                );
-                                console.log(isMobileOpen);
-                            }}
-                            animate={isMobileOpen}
-                            initial="closed"
-                            variants={MobileMenuVariants}
+                            whileTap={{ scale: 0.9 }}
+                            className="NavLinkStyle"
                         >
-                            <List size={40} style={{ color: '#fff' }} />
+                            <h3 style={{ margin: '0px' }}>Stephen Lyons</h3>
+                            <h5 style={{ margin: '0px' }}>Software Engineer</h5>
                         </motion.div>
-                    </div>
+                    </Link>
+                </div>
+                <div {...handlers}>
+                    <motion.div className="grid grid-cols-3 text-white g-2">
+                        <motion.p>Home</motion.p>
+                        <motion.p>Projects</motion.p>
+                        <motion.p>About</motion.p>
+                    </motion.div>
                 </div>
             </div>
-            {isMobileOpen === 'closed' ? (
-                <ul>
-                    <li>
-                        <Link to="/projects" className="noDecor">
-                            <motion.div
-                                variants={MenuVariants}
-                                initial="initial"
-                                animate="animate"
-                                whileTap="tap"
-                                className="NavLinkStyle"
-                            >
-                                <h5 style={{ margin: '0px' }}>PROJECTS</h5>
-                            </motion.div>
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/About" className="noDecor">
-                            <motion.div
-                                className="NavLinkStyle"
-                                whileTap={{ scale: 0.9 }}
-                            >
-                                <h5 style={{ margin: '0px' }}>ABOUT</h5>
-                            </motion.div>
-                        </Link>
-                    </li>
-                </ul>
-            ) : (
-                <></>
-            )}
         </>
     );
 };
